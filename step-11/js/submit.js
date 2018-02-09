80d8ea82f329a892a4de5b8e514a6657f4657ff2
Yonas Adiel Wiguna <yonazadielwiguna@gmail.com>
2018-02-10 04:14:56 +0700

    usage of AJAX post

--- step-11/js/submit.js
+++ step-11/js/submit.js
@@ -1,19 +1,23 @@
 $("#sukses").css('display', 'none');
 
+function success() {
+	$("#form").fadeOut();
+	$("#sukses").fadeIn();
+}
+
 function submit() {
 	var data = {
-		"nim" : $("#nim").val(),
-		"firstName" : $("#firstName").val(),
-		"lastName" : $("#lastName").val(),
-		"email" : $("#email").val(),
-		"nama-barang" : $("#nama-barang").val(),
-		"tanggal-peminjaman" : $("#tanggal-peminjaman").val(),
-		"tanggal-dikembalikan" : $("#tanggal-dikembalikan").val(),
+		"entry.1493879580" : $("#nim").val(),
+		"entry.1406564454" : $("#firstName").val(),
+		"entry.1132547616" : $("#lastName").val(),
+		"entry.979964275"  : $("#email").val(),
+		"entry.795590846"  : $("#nama-barang").val(),
+		"entry.248108895"  : $("#tanggal-peminjaman").val(),
+		"entry.1617995749" : $("#tanggal-dikembalikan").val(),
 	};
 	console.log(data);
-	$.post("http://oprec.arc.itb.ac.id/api/submit.php", data, function(result) {
-		console.log(result);
-		$("#form").fadeOut();
-		$("#sukses").fadeIn();
-	});
+	var url = 'https://docs.google.com/forms/d/e/1FAIpQLSc9ZnnOycBVGu4XQhAbVUXz4Nt8wmD2bf8GrBCtMbHE6tuqKg/formResponse'
+	$.post(url, data)
+	
+	success()''
 }
